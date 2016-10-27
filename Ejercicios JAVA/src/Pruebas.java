32b3ad59e1cb7af57b7ed62afb595412bb92b618
Daniel Garrote Gordon <dgarrotegordon@gmail.com>
2016-10-27 19:00:46 +0200

    Mas cosas

---
+++ Ejercicios JAVA/src/Pruebas.java
@@ -0,0 +1,21 @@
+
+public class Pruebas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+int b=2;
+int a=5;
+int c=6;
+
+System.out.println((-b+ Math.sqrt((b*b)-(4*a*c)))/(2*a));
+System.out.println((-b- Math.sqrt((b*b)-(4*a*c)))/(2*a));
+
+
+
+
+
+	}
+
+}
